f227d9efcb0e8a7139f62cb8d78cba9bc83681a0
Pdb-1807 <purushottambhade18@gmail.com>
2023-04-18 20:49:26 +0530

    collaboration

---
+++ member1-.js
@@ -0,0 +1,8 @@
+ let bag=[];
+let str="naman";
+for(let i=str.length-1;i>=0;i++){
+bag+=str[];
+}
+if (str==bag){
+console.log("palindrome");
+ 
---
+++ member1.js
@@ -0,0 +1,13 @@
+//** Prime problem
+
+let num=34;
+let count=0;
+for(let i=1;i<=34;i++){
+  if(num%i==0){
+   count++;
+}
+if{
+console.log("prime number");
+}
+else{
+console.log("not a prime number");
